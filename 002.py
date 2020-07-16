6b8619e63122b5be618406a67d815a711b14396a
edudawla <65320927+edudawla@users.noreply.github.com>
2020-07-16 12:47:05 -0300

    Add files via upload

--- 002.py
+++ 002.py
@@ -1,10 +1,13 @@
 
 #inserir conteudo ao final
 arquivo = open('novo.txt', 'r')
+lista = arquivo.readlines()
 
 novo = input('Digite a palavra a ser adicionada: ')
+lista.append(f'{novo}\n')
 
-arquivo = open('novo.txt', 'a')
-arquivo.write(novo)
+arquivo = open('novo.txt', 'w')
+for i in lista:
+    arquivo.writelines(i)
 
 arquivo.close()
